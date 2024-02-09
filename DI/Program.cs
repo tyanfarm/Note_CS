d69e3337a46426347f87f1405da4a27a0c5f1c07
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-02-09 15:36:02 +0700

    add get value for Configuration

--- DI/Program.cs
+++ DI/Program.cs
@@ -13,12 +13,21 @@ configBuilder.AddJsonFile("configuration.json");
 
 configurationRoot = configBuilder.Build();
 
-var data1 = configurationRoot.GetSection("MyServiceOptions").GetSection("data1").Value;
-var data2 = configurationRoot.GetSection("MyServiceOptions").GetSection("data2").Value;
+var jsonMyService = configurationRoot.GetSection("MyServiceOptions");
 
-Console.WriteLine($"{data1} in {data2}");
+//MyService
+var services = new ServiceCollection();
 
-// var services = new ServiceCollection();
+services.Configure<MyServiceOptions>(jsonMyService);
+
+services.AddSingleton<MyService>();
+
+var providers = services.BuildServiceProvider();
+
+var myservice = providers.GetService<MyService>();
+
+myservice?.PrintData();
+
 
 // // IClassC temp = new ClassC()
 // services.AddSingleton<Interface.IClassC, Interface.ClassC>();
@@ -27,20 +36,5 @@ Console.WriteLine($"{data1} in {data2}");
 
 // Interface.IClassB? b = providers.GetService<Interface.IClassB>();
 
-// MyService
-// services.Configure<MyServiceOptions>(
-//     (MyServiceOptions options) => {
-//         options.data1 = "Bonjour";
-//         options.data2 = 2024;
-//     }
-// );
 
-// services.AddSingleton<MyService>();
-
-
-// var providers = services.BuildServiceProvider();
-
-// var myservice = providers.GetService<MyService>();
-
-// myservice?.PrintData();
 
